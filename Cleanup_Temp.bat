1aaca7d266a0a6b81cd598e56a9bfa96ba4ce41b
Get Regged <148784927+GetRegged@users.noreply.github.com>
2024-04-29 11:21:35 +0200

    Add files via upload

---
+++ Cleanup_Temp.bat
@@ -0,0 +1,40 @@
+ipconfig /flushdns
+del "%LocalAppData%\Microsoft\Windows\INetCache\." /s /f /q
+del "%AppData%\Local\Microsoft\Windows\INetCookies\." /s /f /q
+del "%temp%" /s /f /q
+del "%AppData%\Discord\Cache\." /s /f /q
+del "%AppData%\Discord\Code Cache\." /s /f /q
+del "%ProgramData%\USOPrivate\UpdateStore" /s /f /q
+del "%ProgramData%\USOShared\Logs" /s /f /q
+del "C:\Windows\System32\SleepStudy" /s /f /q
+del "%WINDIR%\Logs" /s /f /q
+del "%WINDIR%\Installer\$PatchCache$" /s /f /q
+rmdir /S /Q "%WINDIR%\ServiceProfiles\NetworkService\AppData\Local\Microsoft\Windows\DeliveryOptimization"
+rmdir /S /Q "%AppData%\Local\Microsoft\Windows\INetCache\"
+rmdir /S /Q "%AppData%\Local\Microsoft\Windows\INetCookies"
+rmdir /S /Q "%LocalAppData%\Microsoft\Windows\WebCache"
+rmdir /S /Q "%AppData%\Local\Temp\"
+rmdir /S /Q "%AppData%\Discord\Cache"
+rmdir /S /Q "%AppData%\Discord\Code Cache"
+rmdir /S /Q "%SystemDrive%\$GetCurrent"
+rmdir /S /Q "%SystemDrive%\$SysReset"
+rmdir /S /Q "%SystemDrive%\$Windows.~BT"
+rmdir /S /Q "%SystemDrive%\$Windows.~WS"
+rmdir /S /Q "%SystemDrive%\$WinREAgent"
+rmdir /S /Q "%SystemDrive%\OneDriveTemp"
+rd /s /q %WINDIR%\SoftwareDistribution\Download\
+rd /s /q %LocalAppData%\Temp
+rd /s /q %LocalAppData%\Temp\mozilla-temp-files
+rmdir /s /q "%SystemRoot%\System32\SleepStudy"
+rmdir /s /q "%SystemRoot%\System32\SleepStudy >nul 2>&1"
+
+auditpol /set /subcategory:"Filtering Platform Connection" /success:disable /failure:enable
+FOR /F "tokens=1,2*" %%V IN ('bcdedit') DO SET adminTest=%%V
+IF (%adminTest%)==(Access) goto noAdmin
+for /F "tokens=*" %%G in ('wevtutil.exe el') DO (call :do_clear "%%G") >nul 2>nul
+
+FOR /F "tokens=1,2*" %%V IN ('bcdedit') DO SET adminTest=%%V
+IF (%adminTest%)==(Access) goto noAdmin
+for /F "tokens=*" %%G in ('wevtutil.exe el') DO (call :do_clear "%%G")
+
+exit
