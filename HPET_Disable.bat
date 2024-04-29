0cb55f70e595380158c4e506717f7fb64b5500f0
Get Regged <148784927+GetRegged@users.noreply.github.com>
2024-04-29 19:22:52 +0200

    Update HPET_Disable.bat

--- HPET_Disable.bat
+++ HPET_Disable.bat
@@ -1,4 +1,4 @@
-REM HPET: Disable / Dynamictick: Yes / Tscsync: Enhanced (Gave me 680+ FPS boost)
+REM HPET: Disable / Dynamictick: Yes / Tscsync: Enhanced (Gave me huge FPS boost)
 bcdedit /set useplatformclock false
 bcdedit /set disabledynamictick Yes
 bcdedit /set tscsyncpolicy Enhanced
